0de3395a2f53579102f18070644287a91595bda6
leonan <leonanorth3@gmail.com>
2025-07-15 23:56:36 -0300

    first commit

---
+++ index.ts
@@ -0,0 +1,24 @@
+import * as aws from "@pulumi/aws";
+
+const bucket = new aws.s3.BucketV2("primeiro-bucket", {
+  bucket: "primeiro-bucket-pulumi",
+  tags: {
+    IAC: "true",
+  },
+});
+
+const ecr = new aws.ecr.Repository("primeiro-ecr", {
+  name: "primeiro-ecr",
+  imageTagMutability: "IMMUTABLE",
+  tags: {
+    IAC: "true",
+  },
+});
+
+// Export the name of the bucket
+export const bucketName = bucket.id;
+export const bucketInfo = bucket.bucket;
+export const bucketArn = bucket.arn;
+
+export const ecrName = ecr.name;
+export const ecrRepositoryUrl = ecr.repositoryUrl;
