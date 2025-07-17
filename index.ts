c5a5857715fb43bdd57c616f26a2bfe140ef5aab
leonan <leonanorth3@gmail.com>
2025-07-16 23:41:11 -0300

    new: remove s3 bucket

--- index.ts
+++ index.ts
@@ -7,13 +7,6 @@ const firstBucket = new aws.s3.BucketV2("primeiro-bucket", {
   },
 });
 
-const secondBucket = new aws.s3.BucketV2("second-bucket", {
-  bucket: "second-bucket-pulumi",
-  tags: {
-    IAC: "true",
-  },
-});
-
 const ecr = new aws.ecr.Repository("primeiro-ecr", {
   name: "primeiro-ecr",
   imageTagMutability: "IMMUTABLE",
@@ -26,10 +19,6 @@ const ecr = new aws.ecr.Repository("primeiro-ecr", {
 export const firstBucketName = firstBucket.id;
 export const firstBucketInfo = firstBucket.bucket;
 export const firstBucketArn = firstBucket.arn;
-
-export const secondBucketName = secondBucket.id;
-export const secondBucketInfo = secondBucket.bucket;
-export const secondBucketArn = secondBucket.arn;
 
 export const ecrName = ecr.name;
 export const ecrRepositoryUrl = ecr.repositoryUrl;
